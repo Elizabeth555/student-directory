982c25b36af9f8d0e86766aea75777e946af91da
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-09-18 19:49:11 +0100

    refactor

--- directory.rb
+++ directory.rb
@@ -1,55 +1,61 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
     puts "1 Input the students"
     puts "2 Show the students"
     puts "9 Exit"
+end
 
-    selection = gets.chomp
-
-    case selection
+def process(selection)
+  case selection
     when "1"
-       input_students(students)
+       input_students
     when "2"
+      show_students
+     when "9"
+       exit
+     else
+       puts "I don't know what you want"
+     end
+
+   end
+
+def show_students
       print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you want"
-    end
-
-  end
-
+      print_students_list
+      print_footer
 end
 
 
 
-
-
-
-def input_students(students)
+def input_students
   puts "Please enter the names of the students and cohort, seperated by a comma"
   puts "To finish, just hit return three times"
 
 
-  name_cohort = gets.chomp
+  name = gets.chomp
 
 
 while !name.empty? do
-  puts 'cohort'
+  puts 'Cohort?'
   cohort = gets.chomp
   puts "Enter height"
   height = gets.chomp
   puts "Enter hobby"
   hobby = gets.chomp
-  students << {name: name, cohort: cohort, height: height, hobby: hobby}
-  puts "Now we have #{students.count} students"
+  @students << {name: name, cohort: cohort, height: height, hobby: hobby}
+  puts "Now we have #{@students.count} students"
   puts "Enter name"
   name = gets.chomp
 end
-  students
+  @students
 end
 
 def print_header
@@ -59,30 +65,24 @@ def print_header
   puts header
 end
 
-def print(students)
-    students.each_with_index {|student ,index|
+def print_students_list
+    @students.each_with_index {|student ,index|
         puts "#{index +1} #{student[:name]} (#{student[:cohort]} cohort)"
-
       }
+end
 
-    end
-
-def filter_letter(students)
+def filter_letter
   puts "Find Names begining with __"
   letter = gets.chomp
- filter = students.select {|student| student[:name][0] == letter}
+ filter = @students.select {|student| student[:name][0] == letter}
  print(filter)
- end
+end
+end
 
-
-def print_footer(students)
-  if students.count > 1
+def print_footer
   puts "Overall, we have #{students.count} great students".
-  else students.count == 1
-  puts "Overall, we have #{students.count} great student".
-  end
+end
 
-end
-end
+
 
 interactive_menu
