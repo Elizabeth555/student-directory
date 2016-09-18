23183402f7944e1013acb7fa9cca4665596790dd
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-09-18 19:37:33 +0100

    menu

--- directory.rb
+++ directory.rb
@@ -1,32 +1,71 @@
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
+def interactive_menu
   students = []
-  name = gets.chomp
+  loop do
+    puts "1 Input the students"
+    puts "2 Show the students"
+    puts "9 Exit"
 
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+    selection = gets.chomp
+
+    case selection
+    when "1"
+       input_students(students)
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you want"
+    end
+
   end
+
+end
+
+
+
+
+
+
+def input_students(students)
+  puts "Please enter the names of the students and cohort, seperated by a comma"
+  puts "To finish, just hit return three times"
+
+
+  name_cohort = gets.chomp
+
+
+while !name.empty? do
+  puts 'cohort'
+  cohort = gets.chomp
+  puts "Enter height"
+  height = gets.chomp
+  puts "Enter hobby"
+  hobby = gets.chomp
+  students << {name: name, cohort: cohort, height: height, hobby: hobby}
+  puts "Now we have #{students.count} students"
+  puts "Enter name"
+  name = gets.chomp
+end
   students
-  end
+end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------"
+  title =  "The students of Villains Academy"
+  header ="--------------------"
+  puts title
+  puts header
 end
 
 def print(students)
+    students.each_with_index {|student ,index|
+        puts "#{index +1} #{student[:name]} (#{student[:cohort]} cohort)"
 
-  count = 0
-  while count < students.length
-      puts "#{students[0][:name]} (#{students[1][:cohort]} cohort)"
-    count = count + 1
+      }
 
-  end
-end
+    end
 
 def filter_letter(students)
   puts "Find Names begining with __"
@@ -37,10 +76,13 @@ def filter_letter(students)
 
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count > 1
+  puts "Overall, we have #{students.count} great students".
+  else students.count == 1
+  puts "Overall, we have #{students.count} great student".
+  end
+
+end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
